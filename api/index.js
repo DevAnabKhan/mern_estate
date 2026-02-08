8c808821732d9e03cc93cb43ce767e53a64c4108
DevAnabKhan <anabkhan8148@gmail.com>
2026-02-08 13:13:54 +0500

    added pre installations

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ mongoose
     console.log(error);
   });
 
+const __dirname = path.resolve();
 const app = express();
 
 app.use(express.json());
@@ -30,6 +32,11 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 app.use((error, req, res, next) => {
   const statusCode = error.statusCode || 500;
   const message = error.message || "Internal Server Error";
