39fde624d6d2d9c62b3bcac69255f9026cd7432b
DevAnabKhan <anabkhan8148@gmail.com>
2026-02-08 11:48:07 +0500

    completed about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About</h1>
+      <p className="mb-4 text-slate-700">
+        This is a leading real estate agency that specializes in helping clients
+        buy, sell , and rent properties in the most desirable neighbourhood. Our
+        team of experienced agents is dedicated to providing exceptional
+        services and making the buying and selling process as smooth as
+        possible.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our mission is to help our clients achieve their real estate goals by
+        providing expert advice, personalized services, and a deep understanding
+        of the local markets. Whether you are looking to buyc, sell, or rent a
+        property, we are here to help you every step of the way.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our team of agents has a wealth of experience and knowledge in the real
+        estate industry, and we are committed to providing the highest level of
+        services to our clients. We believe that buying or selling a property
+        should be an exciting and rewarding experience, and we are dedicated to
+        making that a reality for each and every one of our clients.
+      </p>
+    </div>
+  );
 }
